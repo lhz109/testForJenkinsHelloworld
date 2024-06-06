13b47c56ea5ce42bf786c3e4dbeb64bcafe4f5d6
liuhanzhi <liuhzhi@authine.com>
2024-06-06 10:26:12 +0800

    测试修改333

--- jenkinsdemo/src/main/java/org/lhz/jenkinsdemo/demos/web/HelloWorldController.java
+++ jenkinsdemo/src/main/java/org/lhz/jenkinsdemo/demos/web/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping
     public String hello() {
-        return "hello world 222";
+        return "hello world 333";
     }
 }
